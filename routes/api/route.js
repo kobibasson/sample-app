ede6448f786f1605f2f361a539a21ea1eaae4230
kobibasson <93kobi@gmail.com>
2020-04-02 10:38:42 +0300

    ok

--- routes/api/route.js
+++ routes/api/route.js
@@ -4,7 +4,6 @@ const router = express()
 router.post('/', async (req, res, err) => {
   let { number } = req.body
   number *= 2 
-  return res.send(`New number is ${number}`)
+  return res.send(`[${os. hostname()}] New number is ${number}`)
 })
-
 module.exports = router
